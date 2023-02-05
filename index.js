9aac75e31f1cc1b8dd16e5917dd12b295e101123
LunarMarathon <lmaytan24@gmail.com>
2023-02-05 21:09:05 +0530

    got cart logic working
    haven't added code for remove; cart part with quantity, subTotal and total is working. 1 eventListener for 3 btns

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 import { menuArray } from "./data.js"
 
-const mainContainer = document.getElementById("mainContainer")
-let menuString = ""
+const menuContainer = document.getElementById("menuContainer")
+const chosenItemsContainer = document.getElementById("chosenItemsContainer")
 
-for(let item of menuArray){
+let menuString = ""
+let chosenItemsArray = []
+let chosenItemsString = ""
+let quantity = {}
+let sum = 0
+
+for (let item of menuArray) {
     menuString += `
     <div class="itemBlock">
         <div class="emoji">${item.emoji}</div>
@@ -12,10 +18,39 @@ for(let item of menuArray){
             <p>${item.ingredients}</p>
             <h4>$${item.price}</h4>
         </div>
-        <button class="addBtn">+</button>
+        <button class="addBtn" id="${item.name}">+</button>
     </div>
     <hr>`
 
 }
-mainContainer.innerHTML = menuString
+menuContainer.innerHTML = menuString
 
+menuContainer.addEventListener("click", handleAddClick)
+
+function handleAddClick(e) {
+    if (document.getElementById(e.target.id).className === "addBtn") {
+        // alert(e.target.id)
+        chosenItemsString = ""
+        sum = 0
+        // console.log(chosenItemsContainer.style.display)
+        // console.log(chosenItemsContainer)
+        chosenItemsArray.push(e.target.id)
+        chosenItemsContainer.style.display = "block"
+        for (let item of menuArray) {
+            // console.log(item.name)
+            let itemArray = chosenItemsArray.filter(chosen => chosen === item.name)
+            // console.log(itemArray.length)
+            sum += item.price * itemArray.length
+            console.log(sum)
+            if (itemArray.length)
+                chosenItemsString += `<div>${itemArray.length} ${item.name} <button class="removeBtn">remove</button>$${item.price * itemArray.length}</div>`
+
+        }
+        for (let item of chosenItemsArray) {
+        }
+        chosenItemsContainer.innerHTML = chosenItemsString + `<div>Total: $${sum}</div>`
+    }
+}
+
+// object, menu items: count
+// item is pushed into array, for of - filter, length 
